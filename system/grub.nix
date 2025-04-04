849b0142c0aa19a14002d346d0a14ed782026b4f
nox <nox@nox-decima.dev>
2025-04-04 08:12:21 +0200

    Add working catppuccin grub theme

--- system/grub.nix
+++ system/grub.nix
@@ -5,4 +5,5 @@
 	boot.loader.grub.enable = true;
 	boot.loader.grub.device = "/dev/nvme0n1";
 	boot.loader.grub.useOSProber = true;
+	boot.loader.grub.theme = ../config/grub/themes/catppuccin-mocha-grub-theme;
 }
