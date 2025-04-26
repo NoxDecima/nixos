7c1b909cba6c46943a0942f8e752e6725f6ac9f9
nox <nox@nox-decima.dev>
2025-04-26 10:22:02 +0200

    Add auto updates.

---
+++ system/update.nix
@@ -0,0 +1,18 @@
+{ pkgs, lib, ... }:
+
+{
+    # Automatic updating
+    system.autoUpgrade = {
+        enable = true;
+        dates = "weekly";
+    };
+
+    # Automatic cleanup
+    nix.gc = {
+        automatic = true;
+        dates = "daily";
+        options = "--delete-older-than 10d";
+    };
+    nix.settings.auto-optimise-store = true;
+
+}
