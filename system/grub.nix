ddfd84e6b342417f4be5bb069827048029c868a4
nox <info@nox-decima.dev>
2025-06-01 19:59:59 +0200

    ♻️ [config] Restructure and clean up Hyprland and Waybar configurations

--- system/grub.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-
-{
-	# Bootloader.
-	boot.loader.grub.enable = true;
-	boot.loader.grub.device = "/dev/nvme0n1";
-	boot.loader.grub.useOSProber = true;
-	boot.loader.grub.theme = ../config/grub/themes/catppuccin-mocha-grub-theme;
-}
