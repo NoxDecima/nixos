d643fa17a9f49b59020751f617f5876503d6870c
nox <nox@nox-decima.dev>
2025-06-07 12:41:56 +0200

    Grub theme fix

--- profile/nox-desktop/configuration.nix
+++ profile/nox-desktop/configuration.nix
@@ -19,7 +19,7 @@
 	boot.loader.grub.enable = true;
 	boot.loader.grub.device = "/dev/nvme0n1";
 	boot.loader.grub.useOSProber = true;
-	boot.loader.grub.theme = ../config/grub/themes/catppuccin-mocha-grub-theme;
+	boot.loader.grub.theme = ../../config/grub/themes/catppuccin-mocha-grub-theme;
 
 	nixpkgs.config.allowUnfree = true;
 
