efca5ee05489ab237a73646b443077a7b98fa97d
nox <info@nox-decima.dev>
2026-03-10 17:10:07 +0100

    ✨ [nixos] Add printing service with default network printer configuration

--- system/printing.nix
+++ system/printing.nix
@@ -1,6 +1,5 @@
 { ... }:
 {
-
     services.printing.enable = true;
 
     services.avahi = {
@@ -13,7 +12,7 @@
     hardware.printers = {
       ensurePrinters = [
           {
-              name = "EPSON Meteory Office";
+              name = "EPSON_Meteory_Office";
               location = "Network";
               deviceUri = "dnssd://EPSON%20WF-2930%20Series._ipp._tcp.local/?uuid=cfe92100-67c4-11d4-a45f-f8255101cf90";
               model = "everywhere";  # Uses driverless IPP Everywhere
